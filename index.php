20516ed9735220d91cdfa14a1b477e1596fbdbe2
MynorEscobar <mynorescobarefpem@gmail.com>
2025-05-26 08:15:53 -0600

    Ejemplo Backend con PHP Básico

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+    <h1>Menu Principal</h1>
+    <a href="basedatos/vista_regiones.php">Regiones</a>
+</body>
+</html>
